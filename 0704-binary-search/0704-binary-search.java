33b98b7251fbd79996911a68109cafca47b9b85a
Bharat singh <145591613+hiteshsinghchandel@users.noreply.github.com>
2026-08-11 13:23:35 +0530

    Time: 0 ms (100%), Space: 48.4 MB (31.62%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,27 +1,28 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        return search(nums,target,0,nums.length-1);
+    public int search(int[] arr, int target) {
+        return bs(arr,target,0,arr.length-1);
 
         
     }
-    static int search(int[] arr,int t ,int s,int e){
-        if (s>e){
+    static int bs(int[] arr,int t ,int s ,int e){
+        if(s>e){
             return -1;
         }
-        int mid = s + (e-s)/2;
-        if(arr[mid]==t){
-            return mid;
+        int m = s+(e-s)/2;
+        if(arr[m]==t){
+            return m;
         }
+        if(arr[s]<arr[m]){
+        if(arr[s]<=t && t<=arr[m]){
 
-        if (arr[s]<arr[mid]){
-            if (t>=arr[s] && t<=arr[mid]){
-                return search(arr,t,s,mid-1);
-            }else {
-                return search(arr,t,mid+1,e);
-            }
+            return bs(arr,t,s,m-1);
+        }else{
+            return bs(arr,t,m+1,e);
         }
-        if (t>=arr[mid]&& t<=arr[e]){
-            return search(arr,t,mid+1,e);
-        }return search(arr,t,s,mid-1);
+        
+        }if(t>=arr[m] && t<= arr[e]){
+            return bs(arr,t,m+1,e);
+        }
+        return bs(arr,t,s,m-1);
     }
-} 
+}
