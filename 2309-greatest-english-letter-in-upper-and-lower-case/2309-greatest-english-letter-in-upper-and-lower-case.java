1d4c055c03ada4d94355cf6104c733d7254e3a2d
Bharat singh <145591613+hiteshsinghchandel@users.noreply.github.com>
2026-08-17 02:17:31 +0530

    Time: 0 ms (100%), Space: 43.3 MB (71.24%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.java
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.java
@@ -1,57 +1,13 @@
 class Solution {
     public String greatestLetter(String s) {
-    String u = "";
-    String l = "";
-    String ans = "";
 
-    for (int i = 0; i < s.length(); i++) {
+        for(char i = 'Z';i>='A';i--){
 
-        if (Character.isUpperCase(s.charAt(i))) {
-            u = u + s.charAt(i);
-        } else {
-            l = l + s.charAt(i);
-        }
-    }
-
-    for (int i = 0; i < u.length(); i++) {
-        for (int j = 0; j < l.length(); j++) {
-
-            if (Character.toLowerCase(u.charAt(i)) == l.charAt(j)) {
-
-                if (ans.isEmpty() || u.charAt(i) > ans.charAt(0)) {
-                    ans = String.valueOf(u.charAt(i));
-                }
+            if(s.indexOf(i) !=-1  &&  s.indexOf(Character.toLowerCase(i)) != -1 ){
+                return String.valueOf(i);
             }
         }
-    }
-
-    return ans;
-
+        return "";
         
     }
-    // static String occ(String s) {
-    // String u = "";
-    // String l = "";
-    // String ans = "";
-
-    // for (int i = 0; i < s.length(); i++) {
-
-    //     if (Character.isUpperCase(s.charAt(i))) {
-    //         u = u + s.charAt(i);
-    //     } else {
-    //         l = l + s.charAt(i);
-    //     }
-    // }
-
-    // for (int i = 0; i < u.length(); i++) {
-    //     for (int j = 0; j < l.length(); j++) {
-
-    //         if (Character.toLowerCase(u.charAt(i)) == l.charAt(j)) {
-    //             ans = ans + u.charAt(i);
-    //         }
-    //     }
-    // }
-
-    // return ans;
-// }
 }
