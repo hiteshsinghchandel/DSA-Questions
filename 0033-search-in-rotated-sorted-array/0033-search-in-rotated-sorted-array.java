b18acdcf29af18056ee3dd61f703acad3a7ce5e4
Bharat singh <145591613+hiteshsinghchandel@users.noreply.github.com>
2026-09-18 17:36:58 +0530

    Time: 0 ms (100%), Space: 43.7 MB (82.21%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,62 +1,56 @@
 class Solution {
-    public int search(int[] nums, int t) {
-        int ans =-1;
-        int a  = pivot(nums);  
-        if (a == -1) {
-    return bs(nums, 0, nums.length - 1, t, true);}
-        int x = bs(nums,0,a,t,true);
-        int y = bs(nums,a+1,nums.length-1,t,true);
-        if(x!=-1){
-            ans=x;
-        }if(y!=-1){
-            ans = y;
-        }return ans;
+    public int search(int[] nums, int target) {
+
+        int p = pivot(nums);
+        if(target >=nums[0] && target<=nums[p]){
+            return bs(nums,0,p,target);
+        }else{
+            return bs(nums,p+1,nums.length-1,target);
         }
-
-        static int bs(int []arr,int s ,int e,int t ,boolean order ){
-        while (s<=e){
-            int mid = s +(e-s)/2;
-            if (arr[mid]==t){
-                return mid;
-            }
-            if (order) {
-    if (arr[mid] > t) {
-        e = mid - 1;
-    } else {
-        s = mid + 1;
+        
     }
-} else {
-    if (arr[mid] > t) {
-        s = mid + 1;
-    } else {
-        e = mid - 1;
-    }
-}
-            }
-        return -1;
-        }
 
-
-
-
-
-         static int pivot(int[] arr){
-        int s= 0;
+    static int pivot(int[] arr){
+        int s = 0;
         int e = arr.length-1;
-        while (s<=e){
-            int m = s +(e-s)/2;
+
+        if(e ==0){
+            return 0;
+        }
+
+        while(s<e){
+            int m = s + (e-s)/2;
+
+           
             if(m<e && arr[m]>arr[m+1]){
-                return m;
-            }else if (m>s && arr[m]<arr[m-1]){
+                return m ;
+            }if(m>s && arr[m]<arr[m-1]){
                 return m-1;
-            } else if (arr[m]<=arr[s]) {
-                e = m-1 ;
-
-            }else{
-                s =m+1;
             }
-        }return -1;
-    
-}
 
+            if(arr[s]<= arr[m]){
+                s = m+1;
+            }else{
+                e = m-1;
+            }
+        }return s;
+    }
+
+    static int bs(int[] arr,int s,int e ,int t ){
+        
+
+        while(s<=e){
+            int m = s + (e-s)/2;
+            if(arr[m]==t){
+                return m;
+            }
+            if(arr[m]>t){
+                e = m-1;
+            }else{
+                s = m+1;
+            }
+        }
+        return -1;
+
+    }
 }
